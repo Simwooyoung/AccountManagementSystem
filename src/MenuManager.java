781c1d9ecda0f59eaa55e3c5a88754f3baa0cee3
Simwooyoung <swy2849@naver.com>
2021-03-15 22:36:39 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Accounts");
+		System.out.println("2. Delete Accounts");
+		System.out.println("3. Edit Accounts");
+		System.out.println("4. View Accounts");
+		System.out.println("5. Exit");
+
+	}
+
+}
