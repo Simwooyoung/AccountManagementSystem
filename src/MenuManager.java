7baed2c5161228f02017a507e38de1f17530013e
Simwooyoung <swy2849@naver.com>
2021-03-23 12:08:40 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -19,7 +19,7 @@ public class MenuManager {
 		    switch(num) {
 		    case 1:
 		    	System.out.print("Date :");
-		    	String accountsdate = input.next();
+		    	String accountdate = input.next();
 		    	System.out.print("Content :");
 		    	String accountname = input.next();
 		    	System.out.print("Income :");
